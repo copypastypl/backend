17f6b89aaf3bdb2e9a72c786fadbe4b1684e854a
ivall <ivallpl@gmail.com>
2021-05-09 18:20:19 +0200

    New tags system

--- apps/posts/models.py
+++ apps/posts/models.py
@@ -3,11 +3,12 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=16, unique=True)
+
+
 class Post(models.Model):
     title = models.CharField(max_length=36)
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    tags = models.CharField(max_length=64)
-
-    def get_tags(self):
-        return self.tags.split(',')
+    tags = models.ManyToManyField(Tag)
--- apps/posts/serializers.py
+++ apps/posts/serializers.py
@@ -1,9 +1,10 @@
 from rest_framework import serializers
 
-from .models import Post
+from .models import Post, Tag
 
 
 class PostSerializer(serializers.ModelSerializer):
+    tags = serializers.SlugRelatedField(many=True, slug_field='name', queryset=Tag.objects.all())
 
     class Meta:
         model = Post
